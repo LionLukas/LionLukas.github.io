7bcf63cee58b354b671c15d5b6f05c68de0ad77d
LionLukas <scrobelukas@gmail.com>
2023-09-25 14:24:25 -0600

    add to project

---
+++ 03 Drawing with loops single and nested/sketch.js
@@ -0,0 +1,13 @@
+// Drawing with loops single and nested
+// Lukas Scrobe
+// Sept/25/2023
+// Generating a single image with loops
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
